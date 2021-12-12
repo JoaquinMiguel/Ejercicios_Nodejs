ddbaa60fd5727ab0e36836689ccbd571c54e2d94
JoaquinMiguel <joacomiguel1408@gmail.com>
2021-12-12 01:24:56 -0300

    tuki

--- Desafios/3/Varios/ej3.js
+++ Desafios/3/Varios/ej3.js
@@ -10,21 +10,39 @@ app.use(express.urlencoded({extended: true}));
 
 app.listen(8083, () =>{ console.log(' server activo')});
 
-const listaP = [{
+const listaM = [{
     nombre: "jorge",
     raza: "manco",
 }];
 
+const listaP =[];
+
 mascotas.get('/mascotas', (req, res) =>{
-    res.send(listaP);
+    res.send(listaM);
 });
 
 mascotas.post('/mascotas', (req, res) =>{
     const {nombre, raza} = req.body;
 
-    listaP.push({nombre: nombre, raza: raza});
-    res.send('GOOOOOOOOOOD');
+    if (typeof nombre === 'string' && typeof raza === 'string'){
+        listaM.push({nombre: nombre, raza: raza});
+        res.status(200).send('Esta todo en orden');
+    }else res.status(400).send('Tipo de dato erroneo');
+    
 })
 
-app.use('/', mascotas);
+personas.get('/personas', (req, res) =>{
+    res.send(listaP);
+})
+
+personas.post('/personas', (req, res) =>{
+    const {nombre, apellido} = req.body;
+
+    if (typeof nombre === 'string' && typeof apellido === 'string'){
+        listaM.push({nombre: nombre, apellido: apellido});
+        res.status(200).send('Esta todo en orden');
+    }else res.status(400).send('Tipo de dato erroneo');
+})
+
+app.use('/mascotas', mascotas);
 
